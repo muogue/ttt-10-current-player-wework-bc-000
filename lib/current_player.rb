d7ee8c69c5d103a2b4cf6a98bd9b4e8f96ceb22c
Vincent Huang <vincent.huang@wework.com>
2018-02-14 00:02:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def current_player(board)
   output = 0
   index = 0
   9.times do
-    output += position_taken?(board, index)
+    output += position_taken?(board, index) ? 1 : 0
     index += 1
   end
   output
