e8b62b4290d99c1a40363a8553048630a764165d
Vincent Huang <vincent.huang@wework.com>
2018-02-14 00:03:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,6 +8,16 @@ def current_player(board)
   output
 end
 
+def turn_count(board)
+  output = 0
+  index = 0
+  9.times do
+    output += position_taken?(board, index) ? 1 : 0
+    index += 1
+  end
+  output
+end
+
 
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
